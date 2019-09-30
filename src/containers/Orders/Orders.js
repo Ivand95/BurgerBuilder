d4c9ba8712087e85fa3ca128ed0a13130cb9ae2b
Ivan <ivand95@outlook.com>
2019-09-30 18:10:37 -0400

    Fetching data from orders

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,14 +1,38 @@
-import React, {Component} from 'react';
-import Order from '../../components/Order/Order';
+/* eslint-disable no-unused-vars */
+import React, { Component } from "react";
+import Order from "../../components/Order/Order";
+import axios from "../../axios-orders";
 
 class Orders extends Component {
-  render(){
-    return(
+  state = {
+    orders: [],
+    loading: true
+  };
+
+  componentDidMount() {
+    axios
+      .get("/orders.json")
+      .then(res => {
+        const fetchedOrders = [];
+        for (let key in res.data) {
+          fetchedOrders.push({
+            ...res.data[key],
+            id: key
+          });
+        }
+        this.setState({ loading: false, orders: fetchedOrders });
+      })
+      .catch(err => {
+        this.setState({ loading: false });
+      });
+  }
+  render() {
+    return (
       <div>
         <Order />
         <Order />
       </div>
-    )
+    );
   }
 }
 
